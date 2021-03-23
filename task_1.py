1b33901b8997cbbcebf7015cdc0ef2c0e5824814
T1MON <derbindima5@gmail.com>
2021-03-23 19:17:39 +0300

    /

--- task_1.py
+++ task_1.py
@@ -2,27 +2,6 @@ import numpy as np
 import math
 import random as rnd
 
-P = np.array([
-#   1       2       3       4       5       6       7       8       9       10      11      12      13      14      15
-    [0.07,  0.44,   0,      0,      0.49,   0,      0,      0,      0,      0,      0,      0,      0,      0,      0   ],  # 1
-    [0,     0.14,   0,      0,      0.12,   0.74,   0,      0,      0,      0,      0,      0,      0,      0,      0   ],  # 2
-    [0,     0.38,   0.16,   0,      0,      0.46,   0,      0,      0,      0,      0,      0,      0,      0,      0   ],  # 3
-    [0,     0,      0.17,   0.17,   0.27,   0.23,   0.16,   0,      0,      0,      0,      0,      0,      0,      0   ],  # 4
-    [0.19,  0,      0,      0.38,   0.13,   0.09,   0,      0,      0,      0,      0.21,   0,      0,      0,      0   ],  # 5
-    [0,     0.23,   0.11,   0,      0,      0.21,   0.3,    0,      0,      0.03,   0.12,   0,      0,      0,      0   ],  # 6
-    [0,     0.42,   0,      0.18,   0,      0.32,   0.08,   0,      0,      0,      0,      0,      0,      0,      0   ],  # 7
-    [0,     0,      0,      0,      0.36,   0,      0,      0.33,   0,      0,      0,      0,      0,      0,      0.31],  # 8
-    [0,     0,      0,      0,      0.09,   0.12,   0.29,   0.16,   0.19,   0.02,   0,      0,      0,      0.04,   0.09],  # 9
-    [0,     0,      0,      0,      0,      0,      0,      0,      0,      0.1,    0,      0.01,   0.38,   0.21,   0.3 ],  # 10
-    [0,     0,      0,      0,      0.23,   0,      0.2,    0,      0,      0,      0.06,   0.23,   0,      0.09,   0.19],  # 11
-    [0,     0,      0,      0,      0,      0,      0,      0.51,   0.42,   0,      0,      0.07,   0,      0,      0   ],  # 12
-    [0,     0,      0,      0,      0,      0,      0,      0,      0.04,   0,      0.35,   0.21,   0.4,    0,      0   ],  # 13
-    [0,     0,      0,      0,      0,      0,      0,      0.28,   0.32,   0.11,   0,      0,      0.25,   0.04,   0   ],  # 14
-    [0,     0,      0,      0,      0,      0,      0,      0,      0,      0,      0.16,   0,      0,      0.75,   0.09],  # 15
-])
-
-A = np.array([0.12, 0.05, 0.11, 0.07, 0.01, 0.01, 0.11, 0.11, 0.03, 0.13, 0.08, 0.03, 0.01, 0.1, 0.03])
-
 
 def print_mx(array: np.ndarray):
     NUM = 3
@@ -33,25 +12,25 @@ def print_mx(array: np.ndarray):
     print()
 
 
-
 SIZE = 5
 TEST = np.array([[rnd.uniform(0, .2) for _ in range(SIZE)] for _ in range(SIZE)])
-print_mx(TEST)
+TEST_VECTOR = np.array([[rnd.uniform(0, .2)] for _ in range(SIZE)])
 
 
 def f_1(p: np.ndarray, k: int) -> np.ndarray:
     return np.linalg.matrix_power(p, k)
 
 
-def f_2(p: np.ndarray, k: int, a: np.ndarray) -> np.ndarray:
+def f_2(p: np.ndarray, a: np.ndarray, k: int) -> np.ndarray:
     return a @ f_1(p, k)
 
 
 def f_3(p: np.ndarray, k: int) -> np.ndarray:
     size = len(p)
+    if k == 0: return np.eye(size)
     res = p.copy()
     for _ in range(k - 1):
-        temp_arr = np.zeros((size, size))
+        temp_arr = np.zeros(p.shape)
         for i in range(size):
             for j in range(size):
                 s = 0
@@ -70,7 +49,7 @@ def f_3_1(p: np.ndarray, k: int) -> np.ndarray:
     return res
 
 
-def f_3_2(p: np.ndarray, accuracy: int = 10) -> np.ndarray:
+def f_3_2(p: np.ndarray, accuracy: int = 200) -> np.ndarray:
     res = np.zeros(p.shape)
     for t in range(1, accuracy + 1):
         res += t * f_3(p, t)
@@ -87,17 +66,17 @@ def f_4(p: np.ndarray, k: int) -> np.ndarray:
         return p
 
 
-def f_4_1(p: np.ndarray, accuracy: int = 20):
-    res = np.zeros(p.shape)
-    for t in range(1, accuracy + 1):
-        res += t * f_4(p, t)
-    return res
-
-
-def f_4_2(p: np.ndarray, k: int) -> np.ndarray:
+def f_4_1(p: np.ndarray, k: int):
     res = np.zeros(p.shape)
     for t in range(1, k + 1):
         res += f_4(p, t)
+    return res
+
+
+def f_4_2(p: np.ndarray, accuracy: int = 20) -> np.ndarray:
+    res = np.zeros(p.shape)
+    for t in range(1, accuracy + 1):
+        res += t * f_4(p, t)
     return res
 
 
--- task_2.py
+++ task_2.py
@@ -7,53 +7,59 @@ _m = 1
 n = 14
 
 
-size = m + n + 1
-P = np.zeros((size, size))
-for i in range(0, m + n):
-    P[i][i + 1] = lam
-    P[i + 1][i] = (i + 1) * _m if i <= m else m * _m
-
-# A
-size = len(P)
-M = P.T - P.sum(0)
-M[-1].fill(1)
-B = np.array([[0]] * (size - 1) + [[1]])
-X = np.linalg.inv(M) @ B
-
-# B
-print("P отказа:", X[-1])
-
-# C
-print("Относительная проп. способность:", 1 - X[-1])
-print("Асболютная проп. способность:", (1 - X[-1]) * lam)
-
-# D
-s = 0
-for i in range(1, n + 1):
-    s += i * X[m + i]
-print("Средняя длина очереди:", s)
+def f_1(lam: int, m: int, _m: int, n: int):
+    size = m + n + 1
+    p = np.zeros((size, size))
+    for i in range(0, m + n):
+        p[i][i + 1] = lam
+        p[i + 1][i] = (i + 1) * _m if i <= m else m * _m
+    return p
 
 
-# E
-s = 0
-for i in range(n + 1):
-    s += (i + 1) / (m * _m) * X[m + i]
-print("Среднее время в очереди:", s)
+def f_2(p: np.ndarray):
+    size = len(p)
+    m = p.T - p.sum(0)
+    m[-1].fill(1)
+    b = np.array([[0]] * (size - 1) + [[1]])
+    return np.linalg.inv(m) @ b
 
-# F
-s0 = 0
-for i in range(1, m + 1):
-    s0 += i * X[i]
-s1 = 0
-for i in range(m + 1, size):
-    s1 += m * X[i]
-print("Среднее количество занятых каналов:", s0 + s1)
 
-# G
-print("Сумма установившихся вероятностей от 0 до m-1", sum(X[0: m]))
-
-# H
-d = []
-for i in P:
-    d.append(round(1 / i.sum(), 3))
-print(d)
+# X = f_2(f_1(lam, m, _m, n))
+#
+# # B
+# print("P отказа:", X[-1])
+#
+# # C
+# print("Относительная проп. способность:", 1 - X[-1])
+# print("Асболютная проп. способность:", (1 - X[-1]) * lam)
+#
+# # D
+# s = 0
+# for i in range(1, n + 1):
+#     s += i * X[m + i]
+# print("Средняя длина очереди:", s)
+#
+#
+# # E
+# s = 0
+# for i in range(n + 1):
+#     s += (i + 1) / (m * _m) * X[m + i]
+# print("Среднее время в очереди:", s)
+#
+# # F
+# s0 = 0
+# for i in range(1, m + 1):
+#     s0 += i * X[i]
+# s1 = 0
+# for i in range(m + 1, size):
+#     s1 += m * X[i]
+# print("Среднее количество занятых каналов:", s0 + s1)
+#
+# # G
+# print("Сумма установившихся вероятностей от 0 до m-1", sum(X[0: m]))
+#
+# # H
+# d = []
+# for i in P:
+#     d.append(round(1 / i.sum(), 3))
+# print(d)
